c18afd90f2f0dd38e2da329aedad58679bfa4103
Ryan Talbert <klaurtar@gmail.com>
2021-03-25 00:33:59 -0500

    make it not ignore build

---
+++ build/precache-manifest.a4c0b4e3461f2cc9eb2cccc357300f6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d42c81cbd95e1608eae3d8c7cc5805e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "396d6b4c6fdcd14f8b67",
+    "url": "/static/css/2.987b09ae.chunk.css"
+  },
+  {
+    "revision": "8dbe9fea31c98eb5fb65",
+    "url": "/static/css/main.b6eec301.chunk.css"
+  },
+  {
+    "revision": "396d6b4c6fdcd14f8b67",
+    "url": "/static/js/2.a0c44802.chunk.js"
+  },
+  {
+    "revision": "29d35255254bbecbac21d2a699ba0d82",
+    "url": "/static/js/2.a0c44802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dbe9fea31c98eb5fb65",
+    "url": "/static/js/main.a9076b2a.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
